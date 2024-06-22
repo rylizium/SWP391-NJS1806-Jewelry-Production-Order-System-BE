a7570e1aa40e8fcc9665f24a443aac287411ce07
rylizium <anhdvtse180643@fpt.edu.vn>
2024-06-22 14:54:40 +0700

    update

--- be-all/JewelryAPI/Repositories/obj/Debug/net8.0/Repositories.AssemblyInfo.cs
+++ be-all/JewelryAPI/Repositories/obj/Debug/net8.0/Repositories.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34b4791ee4ee252dfc4795c470d54ddb95c9863e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6dedb924808f05e4a112b05da94985a074407529")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
